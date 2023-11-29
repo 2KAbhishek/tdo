9bf9d77b3c116c0540d0d70da10bf487f04785a4
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-11-29 22:25:58 +0530

    feat: add todo creation

--- tdo.sh
+++ tdo.sh
@@ -12,11 +12,11 @@ Arguments:
 EOF
 }
 
-todos() {
-    cd "$NOTES_DIR" || return
-    rg -l --sort created --glob '!templates/*' '\[ \]' |
-        fzf --bind "enter:execute($EDITOR {})" --preview 'rg -e "\[ \]" {}'
-    cd - >/dev/null || return
+check_command() {
+    if ! command -v "$1" &>/dev/null; then
+        echo "Error: The $1 command is not available. Make sure it is installed."
+        exit 1
+    fi
 }
 
 search() {
@@ -27,16 +27,32 @@ search() {
     cd - >/dev/null || return
 }
 
-check_command() {
-    if ! command -v "$1" &>/dev/null; then
-        echo "Error: The $1 command is not available. Make sure it is installed."
-        exit 1
+todos() {
+    cd "$NOTES_DIR" || return
+    rg -l --sort created --glob '!templates/*' '\[ \]' |
+        fzf --bind "enter:execute($EDITOR {})" --preview 'rg -e "\[ \]" {}'
+    cd - >/dev/null || return
+}
+
+todo() {
+    cd "$NOTES_DIR" || return
+    year=$(date +%Y)
+    month=$(date +%m)
+    today=$(date +%Y-%m-%d)
+    todo_file="log/$year/$month/$today.md"
+
+    if [ ! -f "$todo_file" ]; then
+        cp notes/templates/todo.md "$todo_file"
     fi
+
+    mkdir -p "$(dirname "$todo_file")"
+    $EDITOR "$todo_file"
+    cd - >/dev/null || return
 }
 
 note() {
     cd "$NOTES_DIR" || return
-    notes_file=notes/"$1".md
+    notes_file="notes/$1.md"
 
     if [ ! -f "$notes_file" ]; then
         cp notes/templates/note.md "$notes_file"
@@ -52,16 +68,16 @@ main() {
     check_command "bat"
 
     case "$1" in
-    -h | --help)
+    -h | --help | h | help)
         display_help
         exit 0
         ;;
-    -t | --todos)
-        todos
+    -s | --search | s | search)
+        search "$2"
         exit 0
         ;;
-    -s | --search)
-        search "$2"
+    -t | --todos | t | todos)
+        todos
         exit 0
         ;;
     "")
