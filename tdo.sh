d9cf4a0a35e71f98b4a6e89f4ff58c9cbf77b82d
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-12-15 10:27:39 +0530

    feat: use commit and new entry

--- tdo.sh
+++ tdo.sh
@@ -44,9 +44,12 @@ commit() {
 
 search() {
     cd "$NOTES_DIR" || return
+
     rg -li --sort path "$1" |
         fzf --bind "enter:execute($EDITOR {})" \
             --preview "bat --style=numbers --color=always --line-range=:500 {} || cat {}"
+
+    commit
     cd - >/dev/null || return
 }
 
@@ -54,15 +57,17 @@ pending_todos() {
     cd "$NOTES_DIR" || return
     rg -l --glob '!*/templates/*' '\[ \]' |
         fzf --bind "enter:execute($EDITOR {})" --preview 'rg -e "\[ \]" {}'
+
+    commit
     cd - >/dev/null || return
 }
 
 new_todo() {
     cd "$NOTES_DIR" || return
-    year=$(date +%Y)
-    month=$(date +%m)
-    today=$(date +%Y-%m-%d)
-    todo_file="log/$year/$month/$today.md"
+    year=$(date +'%Y')
+    month=$(date +'%m')
+    file_name=$(date +'%Y-%m-%d.md')
+    todo_file="log/$year/$month/$file_name"
 
     mkdir -p "$(dirname "$todo_file")"
     if [ ! -f "$todo_file" ]; then
@@ -70,6 +75,8 @@ new_todo() {
         [ -f "$template" ] && cp "$template" "$todo_file"
     fi
     $EDITOR "$todo_file"
+
+    commit
     cd - >/dev/null || return
 }
 
@@ -83,6 +90,11 @@ new_note() {
         [ -f "$template" ] && cp "$template" "$notes_file"
     fi
     $EDITOR "$notes_file"
+
+    commit
+    cd - >/dev/null || return
+}
+
 new_entry() {
     cd "$ENTRY_DIR" || return
     year=$(date +'%Y')
@@ -108,21 +120,20 @@ main() {
     check_command "fzf"
 
     case "$1" in
-    -h | --help | h | help)
-        display_help
-        exit 0
+    -e | --entry | e | entry)
+        new_entry
         ;;
     -f | --find | f | find)
         search "$2"
-        exit 0
+        ;;
+    -h | --help | h | help)
+        display_help
         ;;
     -t | --todo | t | todo)
         pending_todos
-        exit 0
         ;;
     "")
         new_todo
-        exit 0
         ;;
     *)
         new_note "$1"
