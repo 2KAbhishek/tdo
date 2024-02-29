4b12a9c40da321bf4e90f1a8695f07ca1b37e710
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-02-29 08:54:23 +0530

    feat: add install function for fish
    Co-authored-by: Ivan Duran <idurandiaz@gmail.com>

--- install.sh
+++ install.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 # shellcheck disable=2016
 
+install_fish() {
+    if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+        fish -c "fish_add_path $HOME/.local/bin"
+    fi
+    fish -c "set -Ux NOTES_DIR $NOTES_DIR"
+}
+
+install_shell() {
+    exports_file="$1"
+    if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
+        echo 'export PATH="$HOME/.local/bin:$PATH"' >>"$exports_file"
+    fi
+    echo "export NOTES_DIR=$NOTES_DIR" >>"$exports_file"
+}
+
 if [ -z "$EDITOR" ]; then
     echo "Error: EDITOR environment variable not set. Please set it to your preferred text editor."
     exit 1
 fi
 
-case "$SHELL" in
-*/bash) exports_file="$HOME/.bashrc" ;;
-*/zsh) exports_file="$HOME/.zshrc" ;;
-*/fish) exports_file="$HOME/.config/fish/config.fish" ;;
-*) echo "Unsupported shell. Please set the environment variables manually." && exit 1 ;;
-esac
+if [ ! -d "$NOTES_DIR" ]; then
+    NOTES_DIR="$HOME/Projects/notes"
+    mkdir -p "$NOTES_DIR"
+fi
 
 mkdir -p ~/.local/bin
 ln -sfnv "$PWD/tdo.sh" ~/.local/bin/tdo
 
-if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-    echo 'export PATH="$HOME/.local/bin:$PATH"' >>"$exports_file"
-fi
-
-if [ ! -d "$NOTES_DIR" ]; then
-    NOTES_DIR="$HOME/Projects/notes"
-    mkdir -p "$NOTES_DIR"
-    echo "export NOTES_DIR=$NOTES_DIR" >>"$exports_file"
-fi
+case "$SHELL" in
+*/bash) install_shell "$HOME/.bashrc" ;;
+*/zsh) install_shell "$HOME/.zshrc" ;;
+*/fish) install_fish ;;
+*) echo "Unsupported shell. Please set the environment variables manually." && exit 1 ;;
+esac
 
 cp -irv templates "$NOTES_DIR/"
 
 echo "tdo setup completed successfully!"
-echo "Please make sure to reload your shell configuration using 'source $exports_file'."
+echo "Please make sure to reload your shell"
