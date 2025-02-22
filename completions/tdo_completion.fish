6cdd3b40484bd663ed3f4cb5ac230c1869aa09e2
Ivan Duran <idurandiaz@gmail.com>
2025-02-22 21:11:01 +0300

    Update find command

--- completions/tdo_completion.fish
+++ completions/tdo_completion.fish
@@ -1,7 +1,7 @@
 # TDO Fish Tab Completions
 
 function __tdo_complete
-    find $NOTES_DIR -mindepth 1 -not -path '*/\.*' | sed "s|^$NOTES_DIR/||" | sort
+    find "$NOTES_DIR" -type f -not -path '*/\.*' | sed "s|^$NOTES_DIR/||" | sort
 end
 
 complete -c tdo -f -a "(__tdo_complete)"
